9640ecda45c5671651010709efdb1a2d6e60efe4
Trinh Manh Hung <manhung31122004@gmail.com>
2025-08-11 07:28:21 +0700

    11/08/2025

---
+++ ICPC_PTIT_2025_ROUND_3/E.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define ll long long
+#define fi first
+#define se second
+#define sz size()
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define vvl vector<vector<long long>>
+#define faster() ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+const int MOD = 1e9 + 7;
+const int N = 1e6 + 5;
+
+void solve(){
+    ll x, y; cin >> x >> y;
+    
+    if (abs(x - y) <= 1) {
+        cout << "Bob";
+    } else {
+        cout << "Alice";
+    }
+}
+
+void iof(){
+    #ifndef ONLINE_JUDGE
+        freopen("inputf.txt", "r", stdin);
+        // freopen("outputf.txt", "w", stdout);
+    #endif
+}
+
+int main(){
+    iof();
+    faster();
+    int TC = 1; 
+    // cin >> TC;
+    while (TC--){
+      solve();
+    }
+    return 0;
+}
+
+/**
+ *    author:  thienban
+ *    created: 10-08-2025 10:18:35
+**/
+
+/*
+    
+*/
